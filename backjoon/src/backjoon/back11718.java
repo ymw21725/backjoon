9f32d84df90d4dff8fc1f472168cb6e148b2ded6
ymw21725 <ymw21725@gmail.com>
2023-04-14 09:31:13 +0900

    0414백준

---
+++ backjoon/src/backjoon/back11718.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class back11718 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+			
+		}
+		
+		
+
+	}
+
+}
