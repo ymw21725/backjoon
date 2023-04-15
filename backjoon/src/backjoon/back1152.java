954ca815f5cca3c11144b14c6028588968bb9c77
ymw21725 <ymw21725@gmail.com>
2023-04-15 12:08:18 +0900

    0415백준

--- backjoon/src/backjoon/back1152.java
+++ backjoon/src/backjoon/back1152.java
@@ -1,14 +1,19 @@
 package backjoon;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class back1152 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str;
-		str = sc.next();
-		int cnt=0;
+		str = sc.nextLine();
+		
+		
+		StringTokenizer st = new StringTokenizer(str," ");
+		
+		System.out.println(st.countTokens());
 		
 		
 		
