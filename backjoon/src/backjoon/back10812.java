f57399fbffe5ac1de064b2b1b2c4200c4cba075a
ymw21725 <ymw21725@gmail.com>
2023-04-18 09:14:42 +0900

    0418백준

--- backjoon/src/backjoon/back10812.java
+++ backjoon/src/backjoon/back10812.java
@@ -1,19 +1,63 @@
 package backjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.util.StringTokenizer;
 
 public class back10812 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();	// N은 바구니 개수.
-		int M = sc.nextInt();	// M은 회전 수
+	public static void main(String[] args) throws IOException{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+         StringTokenizer token = new StringTokenizer(br.readLine());
+         
+         int N = Integer.parseInt(token.nextToken());
+         int M = Integer.parseInt(token.nextToken());
 		
+		int[] firstarr = new int[N];
+		int[] secondarr = new int[N];
 		
+		for(int p=0; p<firstarr.length; p++) {
+			firstarr[p] = p+1;
+		}
 		
+		for(int z = 0; z<M; z++) {
+			token = new StringTokenizer(br.readLine());
+			
+			int i = Integer.parseInt(token.nextToken())-1;
+			int j = Integer.parseInt(token.nextToken())-1;
+			int k = Integer.parseInt(token.nextToken())-1;
+			
+			
+			int tmp = i;
+			
+			for(int b = 0; b<j-i+1; b++) {
+				if(k+b <= j) {
+					secondarr[b+i] = firstarr[k+b];
+				} else {
+					secondarr[b+i] = firstarr[tmp];
+					tmp++;
+				} 
+			}
+			
+			for(int c = 0; c<N; c++) {
+				if(secondarr[c] != 0) {
+					firstarr[c] = secondarr[c];
+				}
+			}
 		
+		}
+		br.close();
 		
-
+		for(int i = 0; i<N;i++) {
+			bw.write(firstarr[i]+" ");
+		}
+		bw.flush();
+		bw.close();
 	}
 
 }
