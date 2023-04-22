7f65e6a1d34742a7b8c1a3a8df77555666e635d3
ymw21725 <ymw21725@gmail.com>
2023-04-22 10:01:24 +0900

    0422백준

---
+++ backjoon/src/backjoon/back2566.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class back2566 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[9][9];
+		int max = 0;
+		int a=0, b=0;
+		
+		
+		for(int i=0; i<9;i++) {
+			for(int j=0;j<9;j++) {
+				arr[i][j] = sc.nextInt();
+				
+				if(arr[i][j] >= max ) {
+					max = arr[i][j];
+					a = i+1;
+					b = j+1;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.print(a+" "+b);
+		
+		
+	}
+
+}
