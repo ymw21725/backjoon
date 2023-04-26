bc8d333d81c7fae02aed032bbc5f51a514ae909b
ymw21725 <ymw21725@gmail.com>
2023-04-26 09:29:34 +0900

    0426백준

---
+++ backjoon/src/backjoon/back10798.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class back10798 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[][] arr = new char[5][15];
+		
+		for(int i=0;i<5;i++) {
+			String str = br.readLine();
+			
+			for(int j=0;j<str.length();j++) {
+				
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		for(int i=0;i<15;i++) {
+			for(int j=0;j<5;j++) {
+				if(arr[j][i] == ' ' || arr[j][i] == '\0') {
+					continue;
+				}
+				System.out.print(arr[j][i]);
+			}
+		}
+		
+		
+	}
+
+}
