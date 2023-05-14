9957725cfca596459144a995728ef97245241661
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-05-14 16:59:23 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = -1;
+        
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                max = array[i];
+                answer[0] = max;
+                answer[1] = i;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
