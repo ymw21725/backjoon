dd3878e9478b027381be6d0611da733440105a41
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-05-16 12:30:39 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.56 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string = my_string.toLowerCase();
+        char[] arr = my_string.toCharArray();
+        
+        Arrays.sort(arr);
+        answer = new String(arr);
+        
+        return answer;
+    }
+}
