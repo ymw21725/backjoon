84d210473410b126e8eb29ca16a605423e578071
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-05-17 09:15:00 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] s = new String[quiz.length];
+        String[] answer = new String[quiz.length];
+        
+        for(int i=0;i<quiz.length;i++){
+            s = quiz[i].split(" ");
+            
+            if(s[1].equals("-")){
+                if(Integer.parseInt(s[0]) - Integer.parseInt(s[2]) == Integer.parseInt(s[4])){
+                    answer[i] = "O";
+                }else {
+                    answer[i] = "X";
+                }
+            }
+            
+            if(s[1].equals("+")){
+                if(Integer.parseInt(s[0]) + Integer.parseInt(s[2]) == Integer.parseInt(s[4])){
+                    answer[i] = "O";
+                }else {
+                    answer[i] = "X";
+                }
+            }
+            
+            
+            
+        }
+        
+        return answer;
+    }
+}
