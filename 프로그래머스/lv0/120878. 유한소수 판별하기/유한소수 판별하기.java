e3b56bc2c8dc9bf04aa99e8ca7e4deb2eeedfa91
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-05-25 09:04:13 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -16,8 +16,8 @@ class Solution {
     }
 
     private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
+        if (a%b == 0) {
+            return b;
         } else {
             return gcd(b, a % b);
         }
