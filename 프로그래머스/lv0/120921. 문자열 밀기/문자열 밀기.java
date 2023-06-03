1f9158d838f5339a66a349d30184794ba9d80874
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-06-03 09:18:55 +0900

    [level 0] Title: 문자열 밀기, Time: 1.08 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        
+        for(int i=0;i<A.length();i++){
+            if(A.equals(B))
+                return answer;
+            
+            String a = A.substring(A.length()-1);
+            A = a + A.substring(0,A.length()-1);
+            answer++;
+        }
+        
+        
+        return -1;
+    }
+}
