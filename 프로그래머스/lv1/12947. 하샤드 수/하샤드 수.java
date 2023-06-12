dba13283c47dfb0ee8f3e23859c56e9098bb287f
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-06-12 09:17:34 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+       int sum = 0;
+		int a = x;
+      
+		while (a >= 1) {
+			sum += a % 10;
+			a /= 10;
+		}
+
+		if (x % sum == 0) {
+			return true;
+		} else
+			return false;
+  }
+}
