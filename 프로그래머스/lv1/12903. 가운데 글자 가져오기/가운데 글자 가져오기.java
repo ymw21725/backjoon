e17ca2f8301ddca59ffe73f6c6b1a4d31d7f12b6
ymw21725 <120613071+ymw21725@users.noreply.github.com>
2023-06-19 09:36:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        answer = s.substring((s.length()-1)/2, s.length()/2+1);
+        
+        return answer;
+       
+    }
+}
